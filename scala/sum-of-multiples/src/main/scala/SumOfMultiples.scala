26abc4f85aadd9e033248759f37290c8a32047e0
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-11-15 18:02:55 +0000

    my solution to sum of multiples

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,14 +1,12 @@
 object SumOfMultiples {
 
-
-  def sumAcc(factors: Set[Int], range: List[Int], acc: Int): Int = range match {
-    case Nil => acc
-    case x :: xs if factors.exists(x % _ == 0) => sumAcc(factors, xs, acc + x)
-    case _ :: xs => sumAcc(factors, xs, acc)
-  }
-
   def sum(factors: Set[Int], limit: Int): Int = {
-    sumAcc(factors, (1 to limit).toList, 0)
+    def loop(factors: Set[Int], range: List[Int], acc: Int): Int = range match {
+      case Nil => acc
+      case x :: xs if factors.exists(x % _ == 0) => loop(factors, xs, acc + x)
+      case _ :: xs => loop(factors, xs, acc)
+    }
+    loop(factors, (1 until limit).toList, 0)
   }
 }
 
