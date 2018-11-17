a7d456261e36296b93d44452aaeee306d24e0791
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-11-17 06:22:56 +0000

    iteration 2 sum-of-multiples

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,12 +1,7 @@
 object SumOfMultiples {
 
   def sum(factors: Set[Int], limit: Int): Int = {
-    def loop(factors: Set[Int], range: List[Int], acc: Int): Int = range match {
-      case Nil => acc
-      case x :: xs if factors.exists(x % _ == 0) => loop(factors, xs, acc + x)
-      case _ :: xs => loop(factors, xs, acc)
-    }
-    loop(factors, (1 until limit).toList, 0)
+    (1 until limit).filter(n => factors.exists(n % _ == 0)).sum
   }
 }
 
