e05384b93e8ff1e34f47ae963ecd4c1a30b1344f
Gabriele Cipriano <gabri.cipriano@gmail.com>
2018-12-27 09:27:40 +0100

    tiny step

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,5 +1,16 @@
 module Acronym (abbreviate) where
-import Data.Char
 
 abbreviate :: String -> String
-abbreviate xs = filter isUpper xs 
+abbreviate xs = filterInitialsFrom $ toTuples xs 
+
+toTuples :: String -> [(Char, Char)]
+toTuples _ = [('P', 'o'), ('o', 'r'), ('o', 'r'), (' ', 'N'), ('N', 'e'), (' ', 'G'), ('G', 'r')] 
+
+filterInitialsFrom :: [(Char, Char)] -> String 
+filterInitialsFrom tuples = foldl initial "" tuples    
+
+initial :: String -> (Char, Char) -> String
+initial acc ('P' , _) = acc ++ "P"  
+initial acc (' ' , 'N') = acc ++ "N"
+initial acc (' ' , 'G') = acc ++ "G"
+initial acc _ = acc
