7d49af0510e21a5d066c835a95ef26c20a63d3c2
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-01-16 12:56:50 +0000

    expressive version

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,7 +1,25 @@
+
 object Leap {
-  def leapYear(year: Int): Boolean =
-    year % 4 == 0 &&
-      year % 100 != 0 ||
-      (year % 100 == 0 && year % 400 == 0) ||
-      (year % 400 == 0)
+
+  def leapYear(value: Int): Boolean =
+    isYearOf(value) {
+      divisibleBy(4) and
+        not(divisibleBy(100) and not(divisibleBy(400)))
+    }
+
+  implicit class Condition(condition: Int => Boolean) {
+    def and(other: Int => Boolean): Int => Boolean = i => {condition(i) && other(i)}
+
+    def or(other: Int => Boolean): Int => Boolean = int => { condition(int) || other(int)}
+  }
+
+  def not(condition: Int => Boolean): Int => Boolean = !condition(_)
+
+  def divisibleBy(divisor: Int): Int => Boolean = dividend => {
+    dividend % divisor == 0
+  }
+
+  private def isYearOf(year: Int)(condition: Int => Boolean) = {
+    condition(year)
+  }
 }
