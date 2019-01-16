c44667de2776b06dd0713550243dccbbf4e8d38b
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-01-16 15:15:13 +0000

    removed or

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -7,19 +7,16 @@ object Leap {
         not(divisibleBy(100) and not(divisibleBy(400)))
     }
 
-  implicit class Condition(condition: Int => Boolean) {
-    def and(other: Int => Boolean): Int => Boolean = i => {condition(i) && other(i)}
-
-    def or(other: Int => Boolean): Int => Boolean = int => { condition(int) || other(int)}
-  }
-
-  def not(condition: Int => Boolean): Int => Boolean = !condition(_)
-
-  def divisibleBy(divisor: Int): Int => Boolean = dividend => {
-    dividend % divisor == 0
-  }
-
   private def isYearOf(year: Int)(condition: Int => Boolean) = {
     condition(year)
   }
+
+  private def divisibleBy(divisor: Int): Int => Boolean = _ % divisor == 0
+
+  private implicit class Condition(condition: Int => Boolean) {
+    def and(other: Int => Boolean): Int => Boolean = i => {condition(i) && other(i)}
+  }
+
+  private def not(condition: Int => Boolean): Int => Boolean = !condition(_)
 }
+
