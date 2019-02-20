79e06802e2c76f9bef8641ba541f151d0ca3c164
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-02-20 13:59:07 +0000

    corrected typo

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -2,15 +2,15 @@ object Bob {
 
   def response(statement: String): String = statement match {
     case s if isAskingShouting(s) => "Calm down, I know what I'm doing!"
-    case s if isAskingCalmly(s) => "Sure."
+    case s if isAsking(s) => "Sure."
     case s if isShouting(s) => "Whoa, chill out!"
     case s if isSilent(s) => "Fine. Be that way!"
     case _ => "Whatever."
   }
 
-  private def isAskingShouting(s: String) = isShouting(s) && isAskingCalmly(s)
+  private def isAskingShouting(s: String) = isShouting(s) && isAsking(s)
 
-  private def isAskingCalmly(statement: String): Boolean = statement.trim.endsWith("?")
+  private def isAsking(statement: String): Boolean = statement.trim.endsWith("?")
 
   private def isShouting(statement: String): Boolean = {
     statement.toUpperCase == statement && statement.toLowerCase() != statement
